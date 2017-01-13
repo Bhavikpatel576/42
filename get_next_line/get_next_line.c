9c16e8d1938ea27849cfda3f896e9a9922e41994
Bhavik Patel <bpatel@e1z2r3p43.42.us.org>
2017-01-12 17:25:16 -0800

    program that works with 3 line test

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -15,7 +15,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define BUFFSIZE 500
+#define BUFFSIZE 2
 
 /* 
 ** Niave Algo - will read in the file based off the number of bytes. Will search 
@@ -36,6 +36,7 @@ static void prep_line(char **temp, char **line)
 {
 	char *return_line;
 	int linelength;
+
 	linelength = ft_strlen(*temp) - ft_strlen(ft_strchr(*temp, '\n'));
 	return_line = ft_strdupn(*temp, linelength);
 	*line = return_line;
@@ -45,17 +46,22 @@ int get_next_line(const int fd, char **line)
 {
 	char *temp = NULL;
 	char buf[BUFFSIZE + 1];
-	static char *copyline;
+	static char *copyline = NULL;
 	int rf = 0;
 
 	temp = ft_strdup("");
+	if (copyline)
+	{
+		temp = ft_strdup(&ft_strchr(copyline, '\n')[1]); //might need to check for new line in here
+		copyline = &ft_strchr(copyline, '\n')[1];
+	}
 	while (!ft_strchr(buf, '\n') && (rf = read(fd,buf,BUFFSIZE)))
 	{
 		copyline = ft_strdup(buf);
 		temp = ft_strjoin(temp, copyline);
 	}
-	if (rf > 0)
-		prep_line(&temp, line);
+	//printf("%s %s\n",temp,copyline);
+	prep_line(&temp, line);
 	free(temp);
 	return (rf);
 }
@@ -77,10 +83,13 @@ int main(int argc, char *argv[])
 		close(fd);
 	}
 	v = get_next_line(fd, &line);
+	printf("First call: %s", line);
 	free(line);
-	//printf("First call: %s", line);
 	v = get_next_line(fd, &line);
 	printf("Second call: %s", line);
+	free(line);
+	v = get_next_line(fd, &line);
+	printf("third call: %s", line);
 	return (0);
 }
 
